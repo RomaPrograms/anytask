31f10ac6e16fd943018ccc33adade33fe2ec6f5b
Roman <romasemizon@gmail.com>
2020-05-29 18:09:48 +0300

    Added basic structure.

---
+++ src/test/java/com/bsu/famcs/anytask/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsu.famcs.anytask;//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
